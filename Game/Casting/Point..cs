71a46513d7885e13c0fa393a7466ac752d73c71e
johnstennett24 <john.stennett@stencoinc.com>
2022-03-30 17:44:04 -0600

    added the Point class

---
+++ Game/Casting/Point..cs
@@ -0,0 +1,50 @@
+namespace Lost.Game.Casting
+{
+    public class Point
+    {
+        private int x;
+        private int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Point Add(Point other)
+        {
+            int x = this.x + other.GetX();
+            int y = this.y + other.GetY();
+            return new Point(x, y);
+        }
+
+        public bool Equals(Point other)
+        {
+            return this.x == other.GetX() && this.y == other.GetY();
+        }
+
+        public int GetX()
+        {
+            return x;
+        }
+
+        public int GetY()
+        {
+            return y;
+        }
+
+        public Point Reverse()
+        {
+            int x = this.x * -1;
+            int y = this.y * -1;
+            return new Point(x, y);
+        }
+
+        public Point Scale(int factor)
+        {
+            int x = this.x * factor;
+            int y = this.y * factor;
+            return new Point(x, y);
+        }
+    }
+}
